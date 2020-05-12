462454968416453994657a67cb6e7eae3a1ccaa0
Adam Brown <adam.edward.brown@gmail.com>
2020-05-12 11:32:34 -0400

    testing assignment complete

---
+++ src/Card.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import renderer from 'react-test-renderer';
+
+it('renders without crashing',()=>{
+  const div=document.createElement('div');
+  ReactDOM.render(<Card />,div);
+  ReactDOM.unmountComponentAtNode(div)}
+)
+
+it('renders this UI as expected',()=>{
+  const tree = renderer.create(<Card />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
